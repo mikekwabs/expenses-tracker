b41413951bca9287c841d25916f0f509778bffdd
mikekwabs <mikekobby10@gmail.com>
2023-03-04 15:39:54 +0000

    initial setup :dart:

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Expense Tracker</title>
+        <meta name="description" content="An expenses tracker" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <h1> Hello World.</h1>
+        <p> Welcome to my new Home.</p>
+
+      </main>
+    </>
+  )
+}
